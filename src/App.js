42fdbda7688925f8282bfe6e316419316aef178d
thejohnyu <johnyu.programmer@gmail.com>
2020-02-11 21:48:35 -0500

    hooks with theme toggle

--- src/App.js
+++ src/App.js
@@ -1,93 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends React.Component {
-  // Define a state object to hold our app's state
-  state = {
-    // Boolean attribute that will allow us to toggle the switch
-    // Keep the switch on if the theme is dark
-    checked: localStorage.getItem("theme") === "dark" ? true : false,
-    /**
-     * When a user activates the dark theme we will store the value
-     * on localstorage or set default value to light if it is neither dark
-     * nor light
-     */
-    theme: localStorage.getItem("theme")
-  };
+const App = () => {
+  const [checked, setChecked] = useState(
+    localStorage.getItem("theme") === "dark" ? true : false
+  );
 
-  componentDidMount() {
-    // Update the data-theme attribute of our html tag
+  useEffect(() => {
     document
       .getElementsByTagName("HTML")[0]
       .setAttribute("data-theme", localStorage.getItem("theme"));
-  }
+  }, [checked]);
 
-  // Class method allowing us to toggle the theme change
-  toggleThemeChange = () => {
-    const { checked } = this.state;
-    // If theme is light then change to dark
+  const toggleThemeChange = () => {
     if (checked === false) {
-      // Update localstorage
       localStorage.setItem("theme", "dark");
-      /**
-       * The document.getElementsByTagName(...).setAttribute(...)
-       * will only update the value
-       */
-      // Update the data-theme attribute of our html tag
-      document
-        .getElementsByTagName("HTML")[0]
-        .setAttribute("data-theme", localStorage.getItem("theme"));
-      // Update our state
-      this.setState({
-        // Ensure our switch is on if we change to dark theme
-        checked: true
-      });
+      setChecked(true);
     } else {
-      // Update localstorage
       localStorage.setItem("theme", "light");
-      /**
-       * The document.getElementsByTagName(...).setAttribute(...)
-       * will only update the value until the App is mounted and we change
-       * the state of the switch so we will need to introduce
-       * a React lifecycle called ˝componentDidMount()˝
-       */
-      // Update the data-theme attribute of our html tag
-      document
-        .getElementsByTagName("HTML")[0]
-        .setAttribute("data-theme", localStorage.getItem("theme"));
-      // Update our state
-      this.setState({
-        // Ensure our switch is off if we change to light theme
-        checked: false
-      });
+      setChecked(false);
     }
   };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Click the switch to toggle themes</p>
-          <label class="switch">
-            {/* checked attribute is used to determine the state of 
-              checkbox
-              ----------------------------------------------
-              The onChange attribute will toggle our theme change
-            */}
-            <input
-              type="checkbox"
-              checked={this.state.checked}
-              defaultChecked={this.state.checked}
-              onChange={() => this.toggleThemeChange()}
-            />
-            <span class="slider round" />
-          </label>
-        </header>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Click para cambiar el tema</p>
+        <label>
+          <input
+            type="checkbox"
+            defaultChecked={checked}
+            onChange={() => toggleThemeChange()}
+          />
+        </label>
+      </header>
+    </div>
+  );
+};
 
 export default App;
